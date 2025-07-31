f62b106fd04e5b3da703ceb1e61fe14630037f17
Aakash52thade <117812701+Aakash52thade@users.noreply.github.com>
2025-07-31 09:43:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+
+
+document.getElementById("level").addEventListener("click", () => {
+	let element = document.getElementById("level");
+	 let level = 0;
+	while (element) {
+		level++;
+		element = element.parentElement;
+	}
+	alert(`The level of the element is: ${level}`)
+});
+
+
+
